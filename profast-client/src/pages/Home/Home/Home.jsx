df885b2a643abcbde617e07ce6be1103f69d6d34
anamolhasan <mdanamolhasan1998@gmail.com>
2025-06-29 15:18:51 +0600

    benefit section added

--- profast-client/src/pages/Home/Home/Home.jsx
+++ profast-client/src/pages/Home/Home/Home.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 import Banner from '../Banner/Banner'
 import Services from '../services/Services'
 import ClientLogosMarquee from '../clientLogosMarquee/ClientLogosMarquee'
+import Benefits from '../Benefits/Benefits'
 
 
 const Home = () => {
   return (
     <div>
-        <h2>
+
           <Banner />
           <Services />
           <ClientLogosMarquee />
-        </h2>
+          <Benefits />
+     
     </div>
   )
 }
